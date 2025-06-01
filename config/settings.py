fa231e5d5388724dc86b6b543387333bd3f500b2
seyifunmi Odediran <philipoluseyi@gmail.com>
2025-06-01 16:35:34 +0100

    fix

--- config/settings.py
+++ config/settings.py
@@ -155,3 +155,16 @@ LOGGING = {
     },
 }
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.2/howto/static-files/
+
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Enable WhiteNoise for efficient static file serving
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
