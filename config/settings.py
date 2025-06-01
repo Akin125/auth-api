561f6b6e0bc30f20df0ee8533ab3ec38021bcf1e
seyifunmi Odediran <philipoluseyi@gmail.com>
2025-06-01 18:52:53 +0100

    done 5

--- config/settings.py
+++ config/settings.py
@@ -195,7 +195,7 @@ if os.environ.get('EMAIL_HOST'):
     EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
     EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True') == 'True'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', ''),
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
     EMAIL_FROM = os.environ.get('EMAIL_FROM', DEFAULT_FROM_EMAIL)
 
 # JWT Settings for JWT Authentication
